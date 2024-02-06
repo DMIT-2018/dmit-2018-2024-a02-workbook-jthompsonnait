a4688c1e75964ee61d21729599520c385a5538aa
James Thompson <112099604+jthompsonnait@users.noreply.github.com>
2024-02-06 09:48:50 -0700

    End Of Day Feb 6

--- HogWild/HogWildSystem/HogWildExtension.cs
+++ HogWild/HogWildSystem/HogWildExtension.cs
@@ -37,6 +37,16 @@ namespace HogWildSystem
                 //   passing the HogWildContext instance as a parameter.
                 return new WorkingVersionService(context);
             });
+
+            services.AddTransient<CustomerService>((ServiceProvider) =>
+            {
+                //  Retrieve an instance of HogWildContext from the service provider.
+                var context = ServiceProvider.GetService<HogWildContext>();
+
+                // Create a new instance of WorkingVersionsService,
+                //   passing the HogWildContext instance as a parameter.
+                return new CustomerService(context);
+            });
         }
     }
 }
---
+++ HogWild/HogWildSystem/Paginator/PagedResult.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+
+// https://github.com/villainoustourist/Blazor.Pagination/tree/master
+
+
+namespace HogWildSystem.Paginator
+{
+    public class PagedResult<T> : PagedResultBase where T : class
+    {
+        public T[] Results { get; set; }
+    }
+}
